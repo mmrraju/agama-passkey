22fb362c45cf64faa5fdd729b655f0ac63b23164
Mostafejur Rahman <mrraju.ice.iu@gmail.com>
2026-04-21 23:27:15 +0600

    refactor: update ScimFido2Helper.java

--- lib/org/gluu/agama/passkey/ScimFido2Helper.java
+++ lib/org/gluu/agama/passkey/ScimFido2Helper.java
@@ -7,7 +7,6 @@ import org.json.JSONObject;
 import java.io.IOException;
 import java.net.URL;
 import java.util.*;
-import io.jans.agama.engine.script.LogUtils;
 
 import static jakarta.ws.rs.core.MediaType.APPLICATION_JSON;
 
@@ -34,11 +33,11 @@ public class ScimFido2Helper extends ScimWSBase {
             StringJoiner joiner = new StringJoiner("&");
             Map.of("userId", userInum).forEach((k, v) -> joiner.add(k + "=" + v));
             request.setQuery(joiner.toString());
-            LogUtils.log("userId: %",userInum);
-            LogUtils.log("request: %",request);
+            log.info("userId:"+userInum)
+            log.info("request:"+request)
             
             String response = sendRequest(request, true, true).getContentAsJSONObject().toJSONString();
-            LogUtils.log("Response scim fido2 devices: %", response);
+            log.info("Response scim fido2 devices: {}", response);
             JSONObject resObject = new JSONObject(response);
             int count = resObject.getInt("totalResults");
             List<Map<String, String>> mapList = new ArrayList<>();
@@ -51,6 +50,7 @@ public class ScimFido2Helper extends ScimWSBase {
                     result.put("id", item.getString("id"));
                     result.put("displayName", item.has("displayName") ? item.getString("displayName") : "Registered without name");
                     result.put("creationDate", item.getString("creationDate"));
+                    if (item.has("lastAccessTime")) result.put("lastAccessTime", item.getString("lastAccessTime"));
                     if (item.has("deviceData")) {
                         JSONObject deviceData = item.getJSONObject("deviceData");
                         if (deviceData.has("type")) result.put("deviceType", deviceData.getString("type"));
@@ -67,6 +67,24 @@ public class ScimFido2Helper extends ScimWSBase {
         }
     }
 
+    public String deleteDevice(String userId, String deviceId) throws IOException {
+        try {
+            URL url = new URL(apiBase + "/v2/Fido2Devices/" + encode(deviceId));
+            HTTPRequest request = new HTTPRequest(HTTPRequest.Method.DELETE, url);
+            request.setAccept(APPLICATION_JSON);
+            log.debug("Deleting device {} for user {}", deviceId, userId);
+            int status = sendRequest(request, true, true).getStatusCode();
+            if (status == 204 || status == 200) {
+                return "DELETED";
+            }
+            throw new IOException("Delete returned unexpected status: " + status);
+        } catch (IOException e) {
+            throw e;
+        } catch (Exception e) {
+            throw new IOException("Could not delete device: " + e.getMessage(), e);
+        }
+    }
+
     public String updateDevice(String userId, String deviceId, String displayName) throws Exception {
         try {
             URL url = new URL(apiBase + "/v2/Fido2Devices/" + encode(deviceId));
@@ -74,11 +92,11 @@ public class ScimFido2Helper extends ScimWSBase {
             request.setAccept(APPLICATION_JSON);
             request.setContentType(APPLICATION_JSON);
             String body = net.minidev.json.JSONObject.toJSONString(Map.of("userId", userId, "displayName", displayName));
-            LogUtils.log("Request updateDevice body: %", body);
+            log.debug("Request updateDevice body: {}", body);
             request.setQuery(body);
 
             String response = sendRequest(request, true, true).getContentAsJSONObject().toJSONString();
-            LogUtils.log("Response update device: %", response);
+            log.debug("Response update device: {}", response);
             return "UPDATED";
 
         } catch (Exception e) {
